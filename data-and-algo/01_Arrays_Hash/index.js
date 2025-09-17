1fff2c01a93136b78a36e478611c0920e8499890
mosesvk <kaumatule93@gmail.com>
2025-09-17 15:23:23 -0600

    update

--- data-and-algo/01_Arrays_Hash/index.js
+++ data-and-algo/01_Arrays_Hash/index.js
@@ -1,30 +1,9 @@
-const arr = [2, 4, 6, 8]
-//                      --> [192, 96, 64, 48]
+const arr = [2, 4, 6, 8, 9, 1, 11]
 
-const solutionFunction = (nums) => {
-    // [2, 4, 6, 8]
-    const n = nums.length;
-    const res = new Array(n).fill(1);
 
-    for (let i = 1; i < n; i++) {
-        res[i] = nums[i -  1] * res[i - 1]
-    }
-
-    console.log(res);
-    
-
-    let postfix = 1;
-    for (let i = n - 1; i >= 0; i--) {
-        res[i] *= postfix;
-        postfix *= nums[i];
-    }
-    // return res          
-
-    console.log(res);
-    
-
+const mergeSort = (nums) => {
     
 
 }
 
-solutionFunction(arr)
+merg(arr)
