8e75112e1bd109650dd4b0f37a5aa925e9e40a3b
mosesvk <kaumatule93@gmail.com>
2025-09-19 13:40:30 -0600

    update

--- data-and-algo/01_Arrays_Hash/index.js
+++ data-and-algo/01_Arrays_Hash/index.js
@@ -3,8 +3,26 @@ const quickSort = (pairs) => {
   return pairs;
 };
 
-const quickSortHelper = (pairs, s, e) => {
-    
+const quickSortHelper = (arr, s, e) => {
+    if (e - s + 1 <= 1) return 
+
+    let pivot = arr[e]
+    let left = s
+
+    for (let i = s; i < e; i++) {
+        if (arr[i] < pivot) {
+            const temp = arr[left]
+            arr[left] = arr[i]
+            arr[i] = temp
+            left++
+        }
+    }
+
+    arr[e] = arr[left]
+    arr[left] = pivot
+
+    quickSortHelper(arr, s, left - 1)
+    quickSortHelper(arr, left + 1, e)
 }
 
 
