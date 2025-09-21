a9f6a77da2e2ad9b20aef0678cc28930c863dd5d
mosesvk <kaumatule93@gmail.com>
2025-09-21 08:48:53 -0600

    update

--- data-and-algo/07_Trees/index.js
+++ data-and-algo/07_Trees/index.js
@@ -8,3 +8,44 @@ class TreeNode {
 
 // insert, findMinVal, remove
 
+const insert = (root, val) => {
+    if (!root) return new TreeNode(val)
+
+    if (val > root.val) {
+        root.right = insert(root.right, val)
+    } else if (val < root.val) {
+        root.left = insert(root.left, val)
+    }
+    return root
+}
+
+const findMinVal = (root) => {
+    let cur = root
+
+    while (cur && cur.left) {
+        cur = cur.left
+    }
+    return cur
+}
+
+const remove = (root, val) => {
+    if (!root) return null
+
+    if (val > root.val) {
+        root.right = remove(root.right, val)
+    } else if (val < root.val) {
+        root.left = remove(root.left, val)
+    } else {
+        if (!root.left) {
+            return root.right
+        } else if (!root.right) {
+            return root.left
+        } else {
+            let minNode = findMinVal(root.right)
+            root.val = minNode.val
+            root.right = remove(root.right, minNode.val)
+        }
+    }
+
+    return root
+}
