e2b08eab97fe44d491033f7bf1a6bbaee67d5a08
mosesvk <kaumatule93@gmail.com>
2025-12-08 17:48:18 -0700

    update

--- python/main.py
+++ python/main.py
@@ -1,11 +1,23 @@
 import json
+import requests
+from dotenv import load_dotenv
+import os 
 
-with open('data/input.json') as f:
+load_dotenv() 
+
+API_URL = os.getenv('API_URL')
+
+with open('data/input.json') as f: 
     data = json.load(f)
 
-with open("data/output.json", "w") as f:
-    json.dump(data, f, indent=2)
+print('Loaded input data:', data)
 
-json.load()
-json.dumps()
-json.dump()
+res = requests.post(API_URL, json=data)
+
+print('Status:', res.status_code)
+print('Res JSON:', res.json())
+
+with open('data/output.json', 'w') as f:
+    json.dump(res.json(), f, indent=2)
+
+print('Output saved to data/output.json')
