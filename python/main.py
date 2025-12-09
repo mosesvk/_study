56ab7d465a97fe388708917345efbd60c39f2e16
mosesvk <kaumatule93@gmail.com>
2025-12-08 20:13:23 -0700

    update

--- python/main.py
+++ python/main.py
@@ -11,5 +11,36 @@ player = {
     'attempts': 100
 }
 
-with open('data/player.json', 'w') as f:
-    json.dump(player, f, indent=2)
+# with open('data/player.json', 'w') as f:
+#     json.dump(player, f, indent=2)
+
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+
+load_dotenv()
+
+url = os.getenv('REQ_URL')
+
+payload = {
+    'action': 'test', 
+    'value': 123
+}
+
+res = requests.post(url, json=payload)
+
+print('status code:', res.status_code)
+try:
+    data = res.json() 
+    print("Response JSON:")
+    pprint(data)
+
+    with open('data/player.json', 'w') as f: 
+        json.dump(data, f, indent=2)
+
+    print('\nReponse saved to data/player.json')
+except ValueError:
+    print("Not valid JSON from server:")
+    print(res.text)
