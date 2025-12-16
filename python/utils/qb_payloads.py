14be3b192b0e1834c089a660341ec506b03a6e3f
mosesvk <kaumatule93@gmail.com>
2025-12-15 21:26:36 -0700

    update

--- python/utils/qb_payloads.py
+++
@@ -1,9 +0,0 @@
-def build_upsert(table_id, fields, record_id=None):
-    return {
-        'action': 'qb_upsert', 
-        'recordId': record_id, 
-        'payload': {
-            'tableId': table_id, 
-            'fields': fields
-        }
-    }
